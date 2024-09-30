a74e7c21ff77d086fa65361efbac30d3826df50a
FRANKGT241 <mau910462@gmail.com>
2024-09-29 21:04:39 -0600

    Arreglo en productos perecederos, no se podia ver la simagenes de forma correcta.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));  // Límite de 50 MB para JSON
 app.use(express.urlencoded({ limit: '50mb', extended: true }));  // Límite de 50 MB para formularios
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 // Rutas principales
 const clientRoute = require('./routes/clientRoutes.js');
