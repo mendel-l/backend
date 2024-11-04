3d024c5cc2565504d9b7123235efc18fcc72fdff
Keithleen Ivana Ruíz Estrada <125908174+keithre19@users.noreply.github.com>
2024-11-04 09:17:50 -0600

    fix: :bug: fix client status route to use a more descriptive endpoint

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -5,7 +5,7 @@ const authController = require('../controller/authController.js');
 
 router.post('/clients', authController.authenticateToken, ClientController.createClient);
 router.put('/clients/:id', authController.authenticateToken, ClientController.updateClient);
-router.put('/clients/:id', authController.authenticateToken, ClientController.changeStatusClient);
+router.put('/clients/:id/status', authController.authenticateToken, ClientController.changeStatusClient);
 router.get('/clients/:id',authController.authenticateToken, ClientController.findOneClient);
 router.get('/clients',authController.authenticateToken, ClientController.findAllClients);
 
